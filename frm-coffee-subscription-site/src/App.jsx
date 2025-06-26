5b192c11bf4649d0df2b74de14af81dbcd57b1d6
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-06-26 17:56:30 -0400

    create navbar component

--- frm-coffee-subscription-site/src/App.jsx
+++ frm-coffee-subscription-site/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
     </>
   );
 }
---
+++ frm-coffee-subscription-site/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Coffeeroasters</h1>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Create your plan</li>
+      </ul>
+    </nav>
+  );
+}
